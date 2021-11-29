f8c020f2c5a6d4bd8eadadad6fbfbd4f0fc249e4
Siddarth72 <72@siddarth.com>
2021-11-29 19:42:00 +0530

    Server setup and database connection

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require("cookie-parser");
+
+if (process.env.NODE_ENV !== "PRODUCTION") {
+	require("dotenv").config({ path: "backend/config/config.env" });
+}
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+module.exports = app;
---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const app = require('./app.js');
+const databaseConnection = require('./config/database.js');
+const colors = require('colors');
+
+// ! Handle UnCaught Exceptions
+process.on('uncaughtException', (err) => {
+	console.log(`Error: ${err.message}`.red.bold);
+	console.log(`Shutting down the server due to Uncaught Exceptions`.red.bold);
+	process.exit(1);
+});
+
+// ? Config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+	require("dotenv").config({ path: "backend/config/config.env" });
+}
+
+// ? Databse Connection
+databaseConnection();
+
+const server = app.listen(process.env.PORT, () => {
+	console.log(`Server Activated: http://localhost:${process.env.PORT}`.yellow.bold);
+});
+
+// ! Unhandled Promise Rejection
+process.on('unhandledRejection', (err) => {
+	console.log(`Error: ${err.message}`.red.bold);
+	console.log(`Shutting down the server due to Unhandled Promise Rejection`.red.bold);
+	server.close(() => {
+		process.exit(1);
+	});
+});
