dcd8543394fa144e16de320d4c87655625b7118f
Siddarth72 <72@siddarth.com>
2021-12-03 21:22:20 +0530

    Added medicine crud with search

--- backend/controllers/medicineCRUDController.js
+++ backend/controllers/medicineCRUDController.js
@@ -1,12 +1,31 @@
 const Medicine = require('../schema/medicineSchema.js');
+const ApiFeatures = require('../utils/apiFeatures.js');
 
-// ? @desc: Get All Medicines
+// ? @desc: All medicine details
 // ? @route: GET /medapi/medicines
-exports.getAllMedicines = async (req, res, next) => {
+exports.getAllMedicinesDetails = async (req, res, next) => {
 	try {
-		const medicines = await Medicine.find();
+		// const medicines = await Medicine.find();
+		// res.status(200).json({
+		// 	success: true,
+		// 	medicines: medicines,
+		// 	length: medicines.length
+		// });
+
+		const resultPerPage = 2;
+		const medCounts = await Medicine.countDocuments();
+		const apiFeature = new ApiFeatures(Medicine.find(), req.query)
+			.search();
+		let medicines = await apiFeature.query;
+		let filteredMedCounts = medicines.length;
+		apiFeature.pagination(resultPerPage);
+		// medicines = await apiFeature.query;
 		res.status(200).json({
-			medicines: medicines
+			success: true,
+			medicines: medicines,
+			medCounts: medCounts,
+			resultPerPage,
+			filteredMedCounts,
 		});
 	} catch (error) {
 		res.status(500).json({
@@ -15,9 +34,9 @@ exports.getAllMedicines = async (req, res, next) => {
 	}
 };
 
-// ? @desc: Add Medicine
+// ? @desc: Add Medicine details
 // ? @route: POST /medapi/addmedicine
-exports.addMedicine = async (req, res, next) => {
+exports.addMedicineDetails = async (req, res, next) => {
 	try {
 		const medicine = new Medicine({
 			name: req.body.name,
@@ -29,8 +48,81 @@ exports.addMedicine = async (req, res, next) => {
 		});
 		const result = await medicine.save();
 		res.status(201).json({
+			success: true,
 			message: 'Medicine Added Successfully',
 			result: result
+		});
+	} catch (error) {
+		res.status(500).json({
+			message: error.message
+		});
+	}
+};
+
+// ? desc: GET Medicine details By ID
+// ? route: GET /medapi/medicine/:id
+exports.getMedicineDetails = async (req, res, next) => {
+	try {
+		const medicine = await Medicine.findById(req.params.id);
+		if (!medicine)
+			return res.status(404).json({
+				success: false,
+				message: 'Medicine not found'
+			});
+		res.status(200).json({
+			success: true,
+			medicine: medicine
+		});
+	} catch (error) {
+		res.status(500).json({
+			message: error.message
+		});
+	}
+};
+
+// ? desc: Update Medicine details
+// ? route: PUT /medapi/medicine/:id
+exports.updateMedicineDetails = async (req, res, next) => {
+	try {
+		let medicine = await Medicine.findById(req.params.id);
+		if (!medicine)
+			return res.status(404).json({
+				success: false,
+				message: 'Medicine Not Found'
+			});
+		medicine = await Medicine.findByIdAndUpdate(req.params.id, req.body, {
+			new: true,
+			runValidators: true,
+			useFindAndModify: false
+		});
+		await medicine.save();
+		res.status(200).json({
+			success: true,
+			message: 'Medicine Updated Successfully',
+			medicine: medicine
+		});
+	} catch (error) {
+		res.status(500).json({
+			message: error.message
+		});
+	}
+};
+
+// ? @desc: Delete Medicine details
+// ? @route: DELETE /medapi/medicine/:id
+exports.deleteMedicineDetails = async (req, res, next) => {
+	try {
+		const medicine = await Medicine.findById(req.params.id);
+		if (!medicine) {
+			return res.status(404).json({
+				success: false,
+				message: 'Medicine Not Found'
+			});
+		}
+		await Medicine.findByIdAndRemove(req.params.id);
+		res.status(200).json({
+			success: true,
+			message: 'Medicine Deleted Successfully'
 		});
 	} catch (error) {
 		res.status(500).json({
--- backend/routers/medicineRouter.js
+++ backend/routers/medicineRouter.js
@@ -2,14 +2,27 @@ const express = require('express');
 const router = express.Router();
 
 const {
-	getAllMedicines,
-	addMedicine
+	getAllMedicinesDetails,
+	addMedicineDetails,
+	getMedicineDetails,
+	updateMedicineDetails,
+	deleteMedicineDetails
 } = require('../controllers/medicineCRUDController.js');
 
 // ? GET all medicines
-router.get('/medicines', getAllMedicines);
+router.get('/medicines', getAllMedicinesDetails);
 
 // ? POST Add new medicine
-router.post('/addmedicine', addMedicine);
+router.post('/addmedicine', addMedicineDetails);
+
+// ? GET medicine by ID
+router.get('/medicine/:id', getMedicineDetails);
+
+// ? PATCH Update medicine details
+router.put('/medicine/:id', updateMedicineDetails);
+
+// ? DELETE Medicine details
+router.delete('/medicine/:id', deleteMedicineDetails);
+
 
 module.exports = router;
