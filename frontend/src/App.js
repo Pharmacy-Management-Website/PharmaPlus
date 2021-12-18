ad2d0c723d81b9576eb5b6310248f41d9dc035e2
Prabhat Gp <prabhatgp@MacBook-Air.local>
2021-12-18 23:20:55 +0530

    LoginPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,12 @@
-
-import './App.css';
+import LoginPage from "./components/Auth/LoginPage";
+import "./App.css";
 
 function App() {
-
-	return (
-		<div className="App">
-
-		</div>
-	);
+  return (
+    <div className="App">
+      <LoginPage />
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/components/Auth/LoginPage.js
+++ frontend/src/components/Auth/LoginPage.js
@@ -1,12 +1,25 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
+import "./LoginPage.css";
 
 const LoginPage = () => {
-
-	return (
-		<Fragment>
-
-		</Fragment>
-	)
+  return (
+    <Fragment>
+      <div className="login-box">
+        <h2>Welcome! Login</h2>
+        <form>
+          <div className="user-box">
+            <input type="text" name="" required="" />
+            <label>Username</label>
+          </div>
+          <div className="user-box">
+            <input type="password" name="" required="" />
+            <label>Password</label>
+          </div>
+          <button className="btn btn--green">Login</button>
+        </form>
+      </div>
+    </Fragment>
+  );
 };
 
 export default LoginPage;
