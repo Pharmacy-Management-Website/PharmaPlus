6f8c2da3e930c7f09edf1a141d06d2d6cb238815
Prabhat Gp <prabhatgp@MacBook-Air.local>
2021-12-20 15:48:03 +0530

    Login-Page

--- frontend/src/components/Auth/LoginPage.js
+++ frontend/src/components/Auth/LoginPage.js
@@ -1,67 +1,64 @@
 import React, { Fragment, useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import { useAlert } from "react-alert";
 import { loginUser, clearErrors } from "../../actions/userActions.js";
 import "./LoginPage.css";
 
 const LoginPage = () => {
+  const dispatch = useDispatch();
+  const alert = useAlert();
+  const navigate = useNavigate();
 
-	const dispatch = useDispatch();
-	const alert = useAlert();
-	const navigate = useNavigate();
+  const { error, loading, isAuthenticated } = useSelector(
+    (state) => state.user
+  );
 
-	const { error, loading, isAuthenticated } = useSelector((state) => state.user);
+  const [loginUsername, setLoginUsername] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
 
-	const [loginUsername, setLoginUsername] = useState("");
-	const [loginPassword, setLoginPassword] = useState("");
+  const loginSubmit = (e) => {
+    e.preventDefault();
+    dispatch(loginUser(loginUsername, loginPassword));
+    navigate("/home");
+  };
 
-	const loginSubmit = (e) => {
-		e.preventDefault();
-		dispatch(loginUser(loginUsername, loginPassword));
-		navigate("/home");
-	};
+  useEffect(() => {
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
+    }
+    if (isAuthenticated) {
+      navigate("/home");
+    }
+  }, [dispatch, alert, error, isAuthenticated]);
 
-	useEffect(() => {
-		if (error) {
-			alert.error(error);
-			dispatch(clearErrors());
-		}
-		if (isAuthenticated) {
-			navigate("/home");
-		}
-	}, [dispatch, alert, error, isAuthenticated]);
+  return (
+    <Fragment>
+      {/* Login Page */}
 
-	return (
-		<Fragment>
-			<div className="login-box">
-				<h2>Welcome! Login</h2>
-				<form onSubmit={loginSubmit}>
-					<div className="user-box">
-						<input
-							type="text"
-							name=""
-							required
-							value={loginUsername}
-							onChange={(e) => setLoginUsername(e.target.value)}
-						/>
-						<label>Username</label>
-					</div>
-					<div className="user-box">
-						<input
-							type="password"
-							name=""
-							required
-							value={loginPassword}
-							onChange={(e) => setLoginPassword(e.target.value)}
-						/>
-						<label>Password</label>
-					</div>
-					<input type="submit" value="Login" className="btn btn--green" />
-				</form>
-			</div>
-		</Fragment>
-	);
+      <div class="login-box">
+        <h2>Welcome! Login</h2>
+        <form>
+          <div class="user-box">
+            <input type="text" name="" required="" />
+            <label>Username</label>
+          </div>
+          <div class="user-box">
+            <input type="password" name="" required="" />
+            <label>Password</label>
+          </div>
+          <a href="#">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Submit
+          </a>
+        </form>
+      </div>
+    </Fragment>
+  );
 };
 
 export default LoginPage;
--- frontend/src/components/Home/HomePage.js
+++ frontend/src/components/Home/HomePage.js
@@ -1,33 +1,31 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { allMedicines, clearErrors } from '../../actions/medicineActions.js';
+import { allMedicines, clearErrors } from "../../actions/medicineActions.js";
 import { useAlert } from "react-alert";
 
 const HomePage = () => {
+  const dispatch = useDispatch();
+  const alert = useAlert();
 
-	const dispatch = useDispatch();
-	const alert = useAlert();
+  const { error, medicines } = useSelector((state) => state.medicines);
 
-	const { error, medicines } = useSelector((state) => state.medicines);
+  useEffect(() => {
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
+    }
+    dispatch(allMedicines());
+  }, [dispatch, error]);
 
-	useEffect(() => {
-		if (error) {
-			alert.error(error);
-			dispatch(clearErrors());
-		}
-		dispatch(allMedicines());
-	}, [dispatch, error]);
-
-	return (
-		<div>
-			<h1>Home Page</h1>
-			<ul>
-				{medicines.map((medicine) => (
-					<li key={medicine.id}>{medicine.name}</li>
-				))}
-			</ul>
-		</div>
-	);
+  return;
+  <div>
+    <h1>Home Page</h1>
+    <ul>
+      {medicines.map((medicine) => (
+        <li key={medicine.id}>{medicine.name}</li>
+      ))}
+    </ul>
+  </div>;
 };
 
 export default HomePage;
