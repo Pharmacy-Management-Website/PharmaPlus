bb22b5a30d8e1cb59307158b280357ed92a07181
prabhatgp <prabhat.ai20@bmsce.ac.in>
2022-03-11 19:35:01 +0530

    make it better

--- backend/server.js
+++ backend/server.js
@@ -12,14 +12,6 @@ process.on("uncaughtException", (err) => {
 // ? Config
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config({ path: "backend/config/config.env" });
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("Server is running in production mode");
-  });
 }
 
 // ? Database Connection
