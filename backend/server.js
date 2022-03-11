3656761529fdca6ac8bcd12e5bcd51e170b7ed76
prabhatgp <prabhat.ai20@bmsce.ac.in>
2022-03-11 19:01:40 +0530

    heroku

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,43 @@
-const app = require('./app.js');
-const databaseConnection = require('./config/database.js');
-const colors = require('colors');
+const app = require("./app.js");
+const databaseConnection = require("./config/database.js");
+const colors = require("colors");
 
 // ! Handle UnCaught Exceptions
-process.on('uncaughtException', (err) => {
-	console.log(`Error: ${err.message}`.red.bold);
-	console.log(`Shutting down the server due to Uncaught Exceptions`.red.bold);
-	process.exit(1);
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`.red.bold);
+  console.log(`Shutting down the server due to Uncaught Exceptions`.red.bold);
+  process.exit(1);
 });
 
 // ? Config
-if (process.env.NODE_ENV !== "PRODUCTION") {
-	require("dotenv").config({ path: "backend/config/config.env" });
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config({ path: "backend/config/config.env" });
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Server is running in production mode");
+  });
 }
 
-// ? Databse Connection
+// ? Database Connection
 databaseConnection();
 
 const server = app.listen(process.env.PORT, () => {
-	console.log(`Server Activated: http://localhost:${process.env.PORT}`.yellow.bold);
+  console.log(
+    `Server Activated: http://localhost:${process.env.PORT}`.yellow.bold
+  );
 });
 
 // ! Unhandled Promise Rejection
-process.on('unhandledRejection', (err) => {
-	console.log(`Error: ${err.message}`.red.bold);
-	console.log(`Shutting down the server due to Unhandled Promise Rejection`.red.bold);
-	server.close(() => {
-		process.exit(1);
-	});
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`.red.bold);
+  console.log(
+    `Shutting down the server due to Unhandled Promise Rejection`.red.bold
+  );
+  server.close(() => {
+    process.exit(1);
+  });
 });
