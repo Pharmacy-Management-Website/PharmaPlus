c7943b1dda55a4e9f06454356065a2455d975e65
Siddarth72 <72@siddarth.com>
2022-03-14 19:40:38 +0530

    Header fixed

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,101 +3,90 @@ import { logoutUser } from "../actions/userActions";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { useAlert } from "react-alert";
-// import "../styles/main.js";
 
 const Header = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const alert = useAlert();
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+	const alert = useAlert();
 
-  const { manager } = useSelector((state) => state.userLogin);
+	const { manager } = useSelector((state) => state.userLogin);
 
-  const handleLogout = () => {
-    dispatch(logoutUser());
-    alert.success("Logged out successfully");
-    navigate("/");
-  };
+	const handleLogout = () => {
+		dispatch(logoutUser());
+		alert.success("Logged out successfully");
+		navigate("/");
+	};
 
-  const [keyword, setKeyword] = useState("");
+	const [keyword, setKeyword] = useState("");
+	const [click, setClick] = useState(false);
 
-  const handleSearch = (e) => {
-    e.preventDefault();
-    if (keyword.trim()) {
-      navigate(`/medicines/${keyword}`);
-    } else {
-      navigate("/medicines");
-    }
-  };
+	const handleClick = () => setClick(!click);
 
-  return (
-    <div className="container">
-      <div className="navbar-wrapper">
-        {/* Logo */}
-        <a href="/home" className="logo">
-          <i className="fas fa-capsules fa-5x"></i>
-        </a>
+	const handleSearch = (e) => {
+		e.preventDefault();
+		if (keyword.trim()) {
+			navigate(`/medicines/${keyword}`);
+		} else {
+			navigate("/medicines");
+		}
+	};
 
-        {/* Search */}
-        <form className="search" onSubmit={handleSearch}>
-          <input
-            type="text"
-            className="search__input"
-            placeholder="Search Medicines"
-            onChange={(e) => setKeyword(e.target.value)}
-          />
-          <button type="submit" className="search__button">
-            <i className="fa fa-search" aria-hidden="true"></i>
-          </button>
-        </form>
+	return (
+		<div className="container">
+			<div className="navbar-wrapper">
+				{/* Logo */}
+				<a href="/home" className="logo">
+					<i className="fas fa-capsules fa-5x"></i>
+				</a>
 
-        {/* Navbar */}
-        <div className="navbar-btn-group">
-          <ul className="navbar-nav">
-            {
-              manager.role === 'admin' ? (
-                <li>
-                  <a href="/dashboard" className="nav-link">
-                    Dashboard
-                  </a>
-                </li>
-              ) : (
-                <li>
-                  <a href="/invoices" className="nav-link">
-                    Invoices
-                  </a>
-                </li>
-              )
-            }
+				{/* Search */}
+				<form className="search" onSubmit={handleSearch}>
+					<input
+						type="text"
+						className="search__input"
+						placeholder="Search Medicines"
+						onChange={(e) => setKeyword(e.target.value)}
+					/>
+					<button type="submit" className="search__button">
+						<i className="fa fa-search" aria-hidden="true"></i>
+					</button>
+				</form>
 
-
-            <li>
-              <a href="/medicines" className="nav-link">
-                Medicines
-              </a>
-            </li>
-
-            <li>
-              <a href="/cart" className="nav-link">
-                Cart
-              </a>
-            </li>
-
-            <li>
-              <a className="btn primary-btn" onClick={handleLogout} href="/">
-                Logout
-              </a>
-            </li>
-          </ul>
-
-          <button class="menu-toggle-btn">
-            <span class="line one"></span>
-            <span class="line two"></span>
-            <span class="line three"></span>
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+				{/* Navbar */}
+				<div className="navbar-btn-group">
+					<ul className={click ? "nav-menu active" : "nav-menu"}>
+						<li className="nav-item">
+							{
+								manager.role === "admin" && (
+									<a href="/dashboard" className="nav-links">
+										Dashboard
+									</a>
+								)
+							}
+						</li>
+						<li className="nav-item">
+							<a href="/medicines" className="nav-links">
+								Medicines
+							</a>
+						</li>
+						<li className="nav-item">
+							<a href="/cart" className="nav-links">
+								Cart
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-links" onClick={handleLogout} href="/">
+								Logout
+							</a>
+						</li>
+					</ul>
+					<div className="nav-icon" onClick={handleClick}>
+						<i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Header;
